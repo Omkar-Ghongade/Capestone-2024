0498fe05135ff541dc6d38db9623f76f268c6e12
omkar-ghongade <omkarsubhashghongade21@gmail.com>
2024-04-03 23:31:09 +0530

    post project in professor done

--- api/models/project.models.js
+++ api/models/project.models.js
@@ -4,7 +4,7 @@ const projectdata=new mongoose.Schema({
     name:{type:String,required:true},
     professor:{type:String,required:true},
     description : {type:String,required:true},
-    domains : {type:String,required:true},
+    domains : {type:Array,required:true},
     minteamsize : {type:String,required:true},
     maxteamsize : {type:String,required:true},
 });
