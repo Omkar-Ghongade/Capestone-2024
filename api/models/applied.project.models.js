074a868658515d95b58a65f0066d38968b5a6f29
omkar-ghongade <omkarsubhashghongade21@gmail.com>
2024-04-04 00:11:43 +0530

    models changed

--- api/models/applied.project.models.js
+++ api/models/applied.project.models.js
@@ -4,7 +4,7 @@ const appliedprojectdata=new mongoose.Schema({
     applyReason:{type:String,required:true},
     projectId:{type:String,required:true},
     projectName:{type:String,required:true},
-    projectDomain:{type:String,required:true},
+    projectDomain:{type:Array,required:true},
     projectProfessor:{type:String,required:true},
     teamcode:{type:String,required:true},
     studentId:{type:String,required:true}
