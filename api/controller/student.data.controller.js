81ba1da9474d2e9fd82a57642a6edf3c7da3b125
omkar-ghongade <omkarsubhashghongade21@gmail.com>
2024-04-06 22:57:27 +0530

    profile-photo-display

--- api/controller/student.data.controller.js
+++ api/controller/student.data.controller.js
@@ -2,11 +2,16 @@ import studentdata from '../models/student.models.js';
 
 export const getstudentdata = async (req, res) => {
     const email = req.body.emailid;
-    console.log(email);
+    const newPhotoUrl = req.body.url;
+    console.log(req);
     try{
         const user= await studentdata.findOne({emailid:email});
         if(!user) 
             return res.status(404).json({message:"User not found"});
+        if (newPhotoUrl) {
+            user.photo = newPhotoUrl;
+            await user.save(); // Save the updated user
+        }
         return res.status(200).json(user);
     }catch(err){
         res.status(404).json({message:err.message});
