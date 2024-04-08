0a0d32c3af0916e9e8aee7edc941cf4d4b6c60b6
omkar-ghongade <omkarsubhashghongade21@gmail.com>
2024-04-09 01:12:29 +0530

    view reports and bit css in submit report of student

--- client/src/pages/professor/ViewReports.jsx
+++ client/src/pages/professor/ViewReports.jsx
@@ -1,6 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function ViewReports() {
+
+  useEffect(() => {
+    fetchReportsFromBackend();
+  });
+
+  const fetchReportsFromBackend = async () => {
+    try{
+        const name=localStorage.getItem('professorName');
+        const res = await fetch('http://localhost:3000/api/project/getreportlink', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({name:name}),
+        });
+        const data = await res.json();
+        console.log(data);
+    }catch(err){
+        console.log(err);
+    }
+  }
+
   return (
     <div>ViewReports</div>
   )
