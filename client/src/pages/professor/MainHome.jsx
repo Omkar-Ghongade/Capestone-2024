a5a002d09927a4f47284a2002c5a37cdbacaade6
challa-srmap <caditya02@gmail.com>
2024-05-07 22:40:00 +0530

    home page Professor

--- client/src/pages/professor/MainHome.jsx
+++ client/src/pages/professor/MainHome.jsx
@@ -36,15 +36,24 @@ export default function MainHome() {
   }
 
   return (
-    <div className='main-content'>
+    <div className=''>
+      <div><img className="w-full h-screen" src="https://student.srmap.edu.in/srmapstudentcorner/resources/images/Ariel-View-1.jpg" alt="mountain" /></div>
+    <div className="absolute max-sm:mt-16 top-0 left-0 w-full h-full flex justify-center items-center " style={{ fontFamily: 'Helvetica, Arial, sans-serif', textAlign: 'center' }}>
       {professorData && (
-        <div>
-        <p>Name: {professorData.name}</p>
-        <p>Email: {professorData.emailid}</p>
-        <p>Profile Photo: <img  src={professorData.profilephoto} alt="Professor" /></p>
-        <p>ID: {professorData._id}</p>
-      </div>
+        <div className=" rounded-lg p-6 bg-[#4D4D29] bg-opacity-95 shadow-md w-5/12 relative">
+          <div className="rounded-full overflow-hidden mx-auto w-32 md:w-40 h-32 md:h-40 absolute top-[-64px] md:top-[-80px] left-1/2 transform -translate-x-1/2">
+            <img src={professorData.profilephoto} alt="Professor" className="w-full h-full object-cover" />
+          </div>
+          <div className="pt-20 text-white">
+            <h1 className="text-3xl py-2 font-bold">{professorData.name}</h1>
+            <p className="text-xl py-2 ">{professorData.designation}</p>
+            <p className="truncate ">{professorData.emailid}</p>
+            
+          </div>
+        </div>
       )}
+    </div>
     </div>
   )
 }
+
